5f026ef24dc9867d6dff589dd55a2bf7ef99c4d3
DARIA S <DARIA@EXAMPLE.COM>
2023-07-20 22:32:07 +0300

    add result screen

--- src/compareCards.js
+++ src/compareCards.js
@@ -3,6 +3,10 @@ export function compareCards(cards, cardsSet) {
     let firstCard = '';
     let secondCard = '';
     let pairs = 0;
+    let timerId = '';
+    let spentTime = '';
+
+    timerTimeout();
 
     // сравниваем выбранные карты и сообщаем результат
     for (let i = 0; i < cards.length; i++) {
@@ -10,6 +14,9 @@ export function compareCards(cards, cardsSet) {
         card.addEventListener('click', (e) => {
             const target = e.target;
             target.src = cardsSet[i];
+            const gameContainer = document.querySelector('.game_container');
+            let gameHtml = gameContainer.innerHTML;
+            
             if (!firstCard) {
                 firstCard = cardsSet[i];
             } else {
@@ -20,9 +27,35 @@ export function compareCards(cards, cardsSet) {
 
                     if (pairs === cardsSet.length / 2) {
                         alert('Вы выиграли!');
+                        clearTimeout(timerId);
+                        gameHtml += `
+                        <div class="result_container">
+                            <div class="result_block">
+                                <img class="result_image" src="./static/images/celebration.svg" alt="win">
+                                <h2 class="result_title">Вы выиграли!</h2>
+                                <h3 class="result_message">Затраченное время</h3>
+                                <h1 class="result_time">${spentTime}</h1>
+                                <button class="again_button">Играть снова</button>
+                            </div>
+                        </div>`
+
+                        gameContainer.innerHTML = gameHtml;
                     }
                 } else {
                     alert('Вы проиграли!');
+                    clearTimeout(timerId);
+                    gameHtml += `
+                    <div class="result_container">
+                        <div class="result_block">
+                            <img class="result_image" src="./static/images/dead.svg" alt="win">
+                            <h2 class="result_title">Вы проиграли!</h2>
+                            <h3 class="result_message">Затраченное время</h3>
+                            <h1 class="result_time">${spentTime}</h1>
+                            <button class="again_button">Играть снова</button>
+                        </div>
+                    </div>`
+
+                    gameContainer.innerHTML = gameHtml;
                 }
 
                 firstCard = '';
@@ -31,6 +64,7 @@ export function compareCards(cards, cardsSet) {
         });
     }
 
+    // изменение значений на таймере
     function handleTimer() {
         const sec = document.querySelector('.timer_clock_sec');
         const min = document.querySelector('.timer_clock_min');
@@ -47,24 +81,18 @@ export function compareCards(cards, cardsSet) {
             } 
             sec.textContent = '00';
         }
-    
-        // if (sec.textContent == 15) {
-        //     clearInterval(Iteration);
-        //   }
-    }
-    // let Iteration = setInterval(handleTimer, 1000);
+        spentTime = min.textContent + `.` + sec.textContent;
+        return spentTime;
+        }
 
+    // задаем интервал изменений значений на таймере = 1 сек
     function timerInterval () {
-        setInterval(handleTimer, 1000);
+        timerId = setInterval(handleTimer, 1000);
+        return timerId;            
     }
     
+    // запуск таймера через 5сек
     function timerTimeout() {
         setTimeout(timerInterval, 5000);
     }
-    
-    timerTimeout();
 }
-
-
-
-
